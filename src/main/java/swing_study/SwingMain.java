29ff72f14bdc0573902311280261147c45c7a460
parkyujin <ss820938ss@gmail.com>
2021-03-03 12:42:04 +0900

    dd

--- src/main/java/swing_study/SwingMain.java
+++ src/main/java/swing_study/SwingMain.java
@@ -8,6 +8,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import swing_study.component.FrameComponentEx;
+import swing_study.component.JLableEx;
 import swing_study.frame.ContentPaneEx;
 import swing_study.frame.JPanelEx;
 import swing_study.layout.FrameLayout;
@@ -19,6 +20,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.border.TitledBorder;
 import javax.swing.BoxLayout;
+import javax.swing.UIManager;
+import java.awt.Color;
 
 public class SwingMain extends JFrame implements ActionListener {
 
@@ -31,6 +34,10 @@ public class SwingMain extends JFrame implements ActionListener {
 	private JButton btnGridLayout;
 	private JButton btnAbsoluteLayout;
 	private JButton btn03;
+	private JPanel pComponent;
+	private JButton btn04;
+	private JButton btn05;
+	private JButton btn06;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -92,9 +99,27 @@ public class SwingMain extends JFrame implements ActionListener {
 		btn03 = new JButton("JComponent 공통 속성");
 		btn03.addActionListener(this);
 		contentPane.add(btn03);
+		
+		pComponent = new JPanel();
+		pComponent.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "JLable+JButton+JToggle", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		contentPane.add(pComponent);
+		pComponent.setLayout(new GridLayout(0, 1, 0, 1));
+		
+		btn04 = new JButton("JLable");
+		btn04.addActionListener(this);
+		pComponent.add(btn04);
+		
+		btn05 = new JButton("JButton");
+		pComponent.add(btn05);
+		
+		btn06 = new JButton("JToggle");
+		pComponent.add(btn06);
 	}
 
 	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btn04) {
+			actionPerformedBtn04(e);
+		}
 		if (e.getSource() == btn03) {
 			actionPerformedBtn03(e);
 		}
@@ -143,6 +168,10 @@ public class SwingMain extends JFrame implements ActionListener {
 	}
 	protected void actionPerformedBtn03(ActionEvent e) {
 		FrameComponentEx frame = new FrameComponentEx();
+		frame.setVisible(true);
+	}
+	protected void actionPerformedBtn04(ActionEvent e) {
+		JLableEx frame = new JLableEx();
 		frame.setVisible(true);
 	}
 }
---
+++ src/main/java/swing_study/component/JLableEx.java
@@ -0,0 +1,48 @@
+package swing_study.component;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import java.io.File;
+
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.ImageIcon;
+
+public class JLableEx extends JFrame {
+
+	private JPanel contentPane;
+	private String path = System.getProperty("user.dir") + File.separator + "images" + File.separator;
+
+	public JLableEx() {
+		initialize();
+	}
+	private void initialize() {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JLabel lb1 = new JLabel("");
+		lb1.setIcon(new ImageIcon(path+"un3.jpg"));
+		lb1.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lb1, BorderLayout.NORTH);
+		
+		JLabel lb2 = new JLabel("New label");
+		lb2.setIcon(new ImageIcon(path+"un1.jpg"));
+		lb2.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lb2, BorderLayout.CENTER);
+		
+		JLabel lb3 = new JLabel("New label");
+		lb3.setIcon(new ImageIcon(path+"un2.jpg"));
+		lb3.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lb3, BorderLayout.SOUTH);
+	}
+
+}
